336b9c92ef3e57bc1c961ad1031d3ab5d8035cc6
ap3319 <59996946+ap3319@users.noreply.github.com>
2023-02-28 20:53:23 +0000

    change function name

--- src/radians_to_encoder_position.m
+++ src/radians_to_encoder_position.m
@@ -1,4 +1,4 @@
-function encoder = radians_encoder(rad_angle)
+function encoder = radians_to_encoder_position(rad_angle)
     proportion = rad_angle/(2*pi);
     encoder = int32(proportion * 4096);
 end
