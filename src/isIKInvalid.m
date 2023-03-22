97a7d09d5a1e6476b8a384ebd0ccb253779b3a4d
ap3319 <59996946+ap3319@users.noreply.github.com>
2023-03-22 23:34:33 +0000

    Update isIKInvalid.m

--- src/isIKInvalid.m
+++ src/isIKInvalid.m
@@ -1,4 +1,5 @@
 function invalidIK = isIKInvalid(point)
+    % since we append angles to theta in inverse kinematics, simply check if list is long enough
 
     [theta] = InverseKinematics2(point(1),point(2),point(3),point(4));
 
