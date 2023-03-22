becff804b97d0230c6401b5b44e35a6caceb9e3a
ap3319 <59996946+ap3319@users.noreply.github.com>
2023-03-22 23:35:16 +0000

    Update radians_to_encoder_position.m

--- src/radians_to_encoder_position.m
+++ src/radians_to_encoder_position.m
@@ -1,4 +1,4 @@
 function encoder = radians_to_encoder_position(rad_angle)
     proportion = rad_angle/(2*pi);
-    encoder = uint32(proportion * 4096 + 2048);% add 2048 so that angle = 0 is at 
+    encoder = uint32(proportion * 4096 + 2048);% add 2048 so that angle = 0 is at half encoder range
 end
